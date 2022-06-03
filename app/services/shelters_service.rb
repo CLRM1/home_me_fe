b3133794f7250b4db6b183b2d3893e376b8b70bc
craig cardwell <wizeduvcc@gmail.com>
2022-06-02 20:11:07 -0600

    fix: edit shelter service to remove variable from JSON

--- app/services/shelters_service.rb
+++ app/services/shelters_service.rb
@@ -3,7 +3,7 @@ class SheltersService
   def self.get_all_shelters
     connection = Faraday.new(url: "https://desolate-caverns-04440.herokuapp.com/api/v1/")
     response = connection.get("shelters")
-    shetlers = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
