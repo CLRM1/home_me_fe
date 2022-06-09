20e1e05e299700a72389675bdddc6468d8af46d8
Ian Ross <ianrossdrums@gmail.com>
2022-06-09 16:25:17 -0600

    test: add save shelter test

---
+++ spec/features/shelters/save_shelter_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Save shelter" do
+  it "a user can save a shelter from all shelters page" do
+    email = "example@example.com"
+    password = "abc123"
+    # shelters = File.open("./spec/fixtures/shelter.json")
+    # stub_request(:get, "/shelters/nativity-shelter-for-women")
+    #   .to_return(status: 200, body: json_response)
+
+    visit "/"
+    click_on "sign up"
+
+    fill_in "email", with: email
+    fill_in "password", with: password
+    click_on "Submit"
+
+    visit "/shelters"
+
+    within("#shelter-1") do
+      click_button "Add shelter"
+    end
+  end
+end
